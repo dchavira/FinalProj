aa3bcb0ca5a92fcbcecd5d0ba4ef52763b95b807
Damian Chavira <damianchavira@gmail.com>
2021-07-27 21:48:22 -0700

    Fixed cookie bug

--- api/routes/song.js
+++ api/routes/song.js
@@ -5,21 +5,23 @@ const songRouter = express.Router();
 
 const songModel = mongoose.model("songs", songSchema);
 
-songRouter.post("/add/song", (req, res)=>{
-    var song = new songModel({
-        title: req.body.title,
-        album: req.body.album,
-        artist: req.body.artist,
-        image: req.body.image,
-        file: req.body.file
-    })
-    
-    if (!(req.cookies.name)) {
-        res.send("Not logged in").status(401)
-    } else {
-        song.save(function(err){console.log(err)});
-        res.send("Song saved!");
-    }
-})
+songRouter.post("/add/song", (req, res) => {
+  var song = new songModel({
+    title: req.body.title,
+    album: req.body.album,
+    artist: req.body.artist,
+    image: req.body.image,
+    file: req.body.file,
+  });
+  console.log(req.cookies);
+  if (!req.cookies["name"]) {
+    res.send("Not logged in").status(401);
+  } else {
+    song.save(function (err) {
+      console.log(err);
+    });
+    res.send("Song saved!");
+  }
+});
 
 module.exports = songRouter;
