f242d6c9f42bf7ed60ac8bd165e3470506e472dc
Melanie Gin <mgin14@email.arizona.edu>
2021-07-27 19:10:58 -0700

    createdsongapi

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const apiRouter = express.Router();
 const authRouter = require("./routes/auth");
+const songRouter = require("./routes/song");
 
 apiRouter.use("/auth", authRouter);
-
+apiRouter.use("/song", songRouter);
 module.exports = apiRouter;
--- api/schema/song.js
+++ api/schema/song.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var SongSchema = new Schema({
-  title: string,
-  album: string,
-  artist: string,
-  image: image,
-  file: string,
+  title: String,
+  album: String,
+  artist: String,
+  image: String,
+  file: String,
 });
 
 module.exports = SongSchema;
